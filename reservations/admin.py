56b8ae5594fdb5d6b890427c8e95cc95fda36263
jundragon <jjundragon88@gmail.com>
2020-01-13 01:08:56 +0900

    reservation model design

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.ReservationModel)
+class ReservationAdmin(admin.ModelAdmin):
+
+    """ Reservation Admin Definition"""
+
+    pass
